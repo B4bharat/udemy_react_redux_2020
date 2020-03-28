7704c9b2c9e99be60871f54ead780185ba010ff8
Bharat Poptwani <bharat.poptwani@bookmyshow.com>
2020-03-28 16:46:04 +0530

    created first instance of children props

--- components/src/index.js
+++ components/src/index.js
@@ -8,31 +8,32 @@ import ApprovalCard from "./ApprovalCard";
 const App = () => {
   return (
     <div className='ui container comments'>
-      <ApprovalCard />
-      <CommentDetail
-        author='Ayush'
-        avatar={faker.image.avatar()}
-        timeAgo='Today at 4:45PM'
-        text='Learnt a lot'
-      />
-      <CommentDetail
-        author='Aniket'
-        avatar={faker.image.avatar()}
-        timeAgo='Today at 2:00AM'
-        text='You are awesome!!'
-      />
-      <CommentDetail
-        author='Valey'
-        avatar={faker.image.avatar()}
-        timeAgo='Today at 1:45PM'
-        text='You are a life saver!'
-      />
-      <CommentDetail
-        author='Parvez'
-        avatar={faker.image.avatar()}
-        timeAgo='Yesterday at 5:00PM'
-        text='I will leave a critical comment since everyone else is so flowery'
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author='Ayush'
+          avatar={faker.image.avatar()}
+          timeAgo='Today at 4:45PM'
+          text='Learnt a lot'
+        />
+        <CommentDetail
+          author='Aniket'
+          avatar={faker.image.avatar()}
+          timeAgo='Today at 2:00AM'
+          text='You are awesome!!'
+        />
+        <CommentDetail
+          author='Valey'
+          avatar={faker.image.avatar()}
+          timeAgo='Today at 1:45PM'
+          text='You are a life saver!'
+        />
+        <CommentDetail
+          author='Parvez'
+          avatar={faker.image.avatar()}
+          timeAgo='Yesterday at 5:00PM'
+          text='I will leave a critical comment since everyone else is so flowery'
+        />
+      </ApprovalCard>
     </div>
   );
 };
