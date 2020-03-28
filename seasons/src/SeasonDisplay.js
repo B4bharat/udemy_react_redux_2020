fb90d1c361b67c83fed5398ce22aa225cebf07d2
Bharat Poptwani <bharat.poptwani@bookmyshow.com>
2020-03-29 00:11:11 +0530

    added a bit of styling

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -12,12 +12,15 @@ const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
   const displayText =
     season === "winter" ? "Burr, it is chilly" : "Time to hit the beach!";
+  const icon = season === 'winter' ? 'snowflake' : 'sun';
 
   return (
     <div>
+      <i className={`${icon} icon`} />
       <h1>{displayText}</h1>
+      <i className={`${icon} icon`} />
     </div>
-  )
+  );
 };
 
 export default SeasonDisplay;
