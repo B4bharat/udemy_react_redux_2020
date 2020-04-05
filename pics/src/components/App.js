00deaaed02fdc2f568c0650679776395faa1c0b5
Bharat Poptwani <bharat.poptwani@gmail.com>
2020-04-05 12:39:32 +0530

    axios.create demonstration

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,17 +1,15 @@
 import React from "react";
-import axios from "axios";
+
+import unsplash from '../api/unsplash'
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   state = { images: [] };
 
   onSearchSubmit = async term => {
-    const response = await axios.get("https://api.unsplash.com/search/photos", {
+    const response = await unsplash.get("/search/photos", {
       params: {
         query: term
-      },
-      headers: {
-        Authorization: "Client-ID rmHelButjcptGMxi1Sd_xxoCzABUWIzeiecpyrSLCbk"
       }
     });
 
