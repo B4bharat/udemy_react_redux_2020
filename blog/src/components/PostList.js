ce2b7429e481a1c614d9aeb24cbd35918f574c87
Bharat Poptwani <bharat.poptwani@gmail.com>
2020-04-11 14:30:01 +0530

    added UserHeader component and usersReducer to fetch 'author' data

--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import UserHeader from "./UserHeader";
 import { fetchPosts } from "../actions";
 
 class PostList extends Component {
@@ -17,6 +18,7 @@ class PostList extends Component {
               <h2>{post.title}</h2>
               <p>{post.body}</p>
             </div>
+            <UserHeader userId={post.userId} />
           </div>
         </div>
       );
